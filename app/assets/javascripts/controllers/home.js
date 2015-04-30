76162a3f0249c9c4287603b6d9278882c10a4e34
afrankel <afrankel@solutionstreet.com>
2015-04-30 19:51:16 -0400

    set the error status on the page

--- app/assets/javascripts/controllers/home.js
+++ app/assets/javascripts/controllers/home.js
@@ -1,17 +1,21 @@
 angular.module('limitedBandwith')
-    .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+    .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
 
-      $scope.apicalls = []; // storage for my api call results
+  $scope.apicalls = []; // storage for my api call results
 
-      // loop through n times to call my api
-      for (var i = 0; i < 10; i++) {
-        $scope.apicalls[i] = {'result':'Started'};
+  // loop through n times to call my api
+  for (var i = 0; i < 10; i++) {
+    $scope.apicalls[i] = {'result':'Started'};
 
-        (function (cntr) { // need function closure here to pass in i
+    (function (cntr) { // need function closure here to pass in i
 
-          $http.get('api_call.json?t=15').success(function (data, i) { // call api that does some work (i.e., sleeps for a bit)
-            $scope.apicalls[cntr].result = data;
-          });
-        })(i);
-      }
-    }]);
+      $http.get('api_call.json?t=15').
+        success(function (data, i) { // call api that does some work (i.e., sleeps for a bit)
+          $scope.apicalls[cntr].result = data;
+        }).
+        error(function (data, status, headers, config) {
+          $scope.apicalls[cntr].result = status; // just set the status on the page so we can see
+        });
+    })(i);
+  }
+}]);
