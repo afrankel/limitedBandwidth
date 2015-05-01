91b3db7393a1fb6493f710a0d2b481ddab5b4252
afrankel <afrankel@solutionstreet.com>
2015-05-01 12:39:42 -0400

    add retry interceptor

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,31 @@
 angular
     .module('limitedBandwith', [
-        'ui.router',
-        'templates'
-    ]).config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-      $stateProvider.state('home', {
-        url: '/',
-        templateUrl : 'home.html',
-        controller: 'HomeCtrl'
-      });
-      // default fall back route
-      $urlRouterProvider.otherwise('/');
-      //$locationProvider.html5Mode(true);
-    }]);
+  'ui.router',
+  'templates'
+]).config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
+  $stateProvider.state('home', {
+    url:'/',
+    templateUrl:'home.html',
+    controller:'HomeCtrl'
+  });
+  // default fall back route
+  $urlRouterProvider.otherwise('/');
+  //$locationProvider.html5Mode(true);
+  var interceptor = ['$injector', '$q', function ($injector, $q) {
+    return {
+      'responseError': function(response) {
+        if (response.status === 0) { // this is a timeout or connection lost
+          // should retry
+          var $http = $injector.get('$http');
+          return $http(response.config);
+        }
+        // give up - some other error
+        return $q.reject(response);
+      }
+    };
+  }];
+
+  $httpProvider.interceptors.push(interceptor);
+}
+
+]);
